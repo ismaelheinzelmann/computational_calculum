3e4f864e8dbb5e27b912d2f8c3711d03abd0c759
ismael <ismaelheinzelmann@gmail.com>
2023-03-21 16:44:04 -0300

    changes

--- linear_system/linear_system_gauss.m
+++ linear_system/linear_system_gauss.m
@@ -1,9 +1,17 @@
 # Sistemas lineares por eliminação gaussiana
-
-A = [1 2 1 ;
-    2 6 3 ;
-    1 2 2]
-B = [8; 26; 11]
+output_precision(16)
+A = [0.448 0.832 0.193 ;
+    0.421 0.784 -0.207 ;
+    -0.319 0.884 0.279]
+B = [1; 0; 0]
 
 # Solução por eliminação gaussiana
 X = met_gauss(A, B)
+
+for x = rows(A) : 1
+    erro = abs(sum(A(x, :) * X') - B(x, 1))
+    print(erro)
+endfor
+
+
+# verificar algoritmo de thomas
--- linear_system/met_gauss.m
+++ linear_system/met_gauss.m
@@ -2,26 +2,25 @@ function X = met_gauss(A, B)
     A = [A B]
     n = rows(A)
 
-    # pivotação inicial
-    for k = 1: n-1
-        [A_max, pos] = max(abs(A(k: n, k)))
-
-        LK = A(k, :) # valor preservado da original
-        A(k,:) = A(pos + k - 1,:)
-
-        A(pos + k - 1, :) = LK
-    endfor
-    # fim pivotacao inicial
-
     # triangulação
     for k = 1:n-1
+    
     # neste caso deve-se escolher a melhor linha para eliminação
     # melhor linha será a que tem o maior valor em sua diagonal principal
     # acumulando menores erros de arredondamento
+    # processo de pivotação inicial
+
+    # início escolhga melhor linha
+    [A_max, pos] = max(abs(A(k: n, k))) # A(i varia de k até n, e j é constante), sendo assim A(i..k, k)
+
+    LK = A(k, :) # valor preservado da original
+    A(k,:) = A(pos + k - 1,:)
+    A(pos + k - 1, :) = LK
+    # fim escolha melhor linha
         for i = k+1:n
             aux = A(i,k)/A(k,k)
             for j = k:n+1
-                A(i,j) = A(i,j) - aux*A(k,j)
+                    A(i,j) = A(i,j) - aux*A(k,j)
             endfor
         endfor
     endfor
@@ -31,16 +30,8 @@ function X = met_gauss(A, B)
     solution = []
     solution(n) = A(n, n+1) / A(n, n)
     for i = n-1 : -1 :1
-        sum = 0
-        for j = i + 1: n
-
-            sum += A(i, j) * solution(j)
-
-        endfor
-
-
+        sum = A(i,1:n) * solution' # solução transposta para coluna, possibilitando multiplicação sem iteração direta
         solution(i) = (A(i, n + 1) - sum)/ A(i, i)
-
     endfor
     # fim retrossubstituição
     
